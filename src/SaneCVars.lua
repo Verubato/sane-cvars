f2b94db93bb0c3ee81101a81b0e5a86be561223f
Verubato <118912121+Verubato@users.noreply.github.com>
2026-01-12 16:43:58 +1100

    Some more cvars.

--- src/SaneCVars.lua
+++ src/SaneCVars.lua
@@ -22,9 +22,45 @@ local function Init()
 		{ Name = "nameplateShowFriendlyGuardians", Value = 0 },
 		{ Name = "nameplateShowFriendlyTotems", Value = 0 },
 
-		-- misc
+		-- don't auto place new spells to action bars
 		{ Name = "AutoPushSpellToActionBar", Value = 0 },
+
+		-- hide the "new profession skill" tooltip
 		{ Name = "hideHelptips", Value = 1, IsSession = true },
+
+		-- hear more audio events
+		{ Name = "Sound_NumChannels", Value = 128 },
+
+		-- class colour unit frames
+		{ Name = "raidFramesDisplayClassCOlor", Value = 1 },
+		{ Name = "pvpFramesDisplayClassColor", Value = 1 },
+
+		-- disable tutorials
+		{ Name = "showTutorials", Value = 0 },
+
+		-- disable error speech
+		{ Name = "Sound_EnableErrorSpeech", Value = 0 },
+
+		-- disable pet sounds
+		{ Name = "Sound_EnablePetSounds", Value = 0 },
+
+		-- camera zoom
+		{ Name = "cameraDistanceMaxZoomFactor", Value = 2.6 },
+
+		-- enable motion sickness setting
+		{ Name = "CameraReduceUnexpectedMovement", Value = 1 },
+
+		-- disable sticky select
+		{ Name = "deselectOnClick", Value = 1 },
+
+		-- combine bags view
+		{ Name = "combinedBags", Value = 1 },
+
+		-- cast on mouseover raid frames and nameplates
+		{ Name = "enableMouseoverCast", Value = 1 },
+
+		-- enable target of target frames
+		{ Name = "showTargetOfTarget", Value = 1 },
 	}
 
 	for _, cvar in ipairs(cvars) do
